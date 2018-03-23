916ed676c9df5948862562b8cf4fa7ec9df273f2
Jake J <jakejohnson818@gmail.com>
2018-03-23 09:06:06 -0500

    Changed code for the for_loops exercise on repeating number

--- js/for_loops.js
+++ js/for_loops.js
@@ -34,29 +34,33 @@ for (var value = 1; value <=10; value++) {
 //     return givenNumber.toString();
 // }
 
-for (var value = 1; value <=9; value++) {
+// for (var value = 1; value <=9; value++) {
+//
+//     if (value == 1) {
+//         console.log(1);
+//     } else if (value == 2) {
+//         console.log(22)
+//     } else if (value == 3) {
+//         console.log(333)
+//     } else if (value == 4) {
+//         console.log(4444)
+//     } else if (value == 5) {
+//         console.log(55555)
+//     } else if (value == 6) {
+//         console.log(666666)
+//     } else if (value == 7) {
+//         console.log(7777777)
+//     } else if (value == 8) {
+//         console.log(88888888)
+//     } else if (value == 9) {
+//         console.log(999999999)}
+// }
+//
+// for (var value = 100; value >=5; (value-=5)) {
+//     console.log(value);
+// }
 
-    if (value == 1) {
-        console.log(1);
-    } else if (value == 2) {
-        console.log(22)
-    } else if (value == 3) {
-        console.log(333)
-    } else if (value == 4) {
-        console.log(4444)
-    } else if (value == 5) {
-        console.log(55555)
-    } else if (value == 6) {
-        console.log(666666)
-    } else if (value == 7) {
-        console.log(7777777)
-    } else if (value == 8) {
-        console.log(88888888)
-    } else if (value == 9) {
-        console.log(999999999)}
+for (var value=1; value<10; value++) {
+    var aString=value.toString();
+    console.log(aString.repeat(value));
 }
-
-for (var value = 100; value >=5; (value-=5)) {
-    console.log(value);
-}
-
