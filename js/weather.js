01fe93737ce621cee0aa3d8a4f17f7c189fbf794
Jake J <jakejohnson818@gmail.com>
2018-04-11 14:54:57 -0500

    removed lat/long search added google maps

--- js/weather.js
+++ js/weather.js
@@ -10,7 +10,6 @@
     });
 
     current.done(function (data) {
-        console.log(data);
         $("#current").append("<div class='current'><p class='temp'>" + data.main.temp_max + "\xB0" + "/" + data.main.temp_min + "\xB0" + "</p>"
             + "<p>" + "<img src='http://openweathermap.org/img/w/" + data.weather[0].icon + ".png'" + "</p>"
             + "<p><span class='weather'>" + data.weather[0].main + ": " + "</span>" + data.weather[0].description + "</p>"
@@ -36,7 +35,6 @@
 
 
     request.done(function (response) {
-        console.log(response);
         response.list.forEach(function (forecast, i) {
             if (i > 0) {
                 $("#forecast").append("<div class='forecast" + i + "'><p class='temp'>" + forecast.temp.max + "\xB0" + "/"
@@ -61,9 +59,6 @@
 
         var latitude = $(".lat").val();
         var longitude = $(".long").val();
-
-        console.log(latitude);
-        console.log(longitude);
 
         var current = $.get("http://api.openweathermap.org/data/2.5/weather", {
             APPID: "8f32c0149a278cdb5f995fbb3d98eba5",
@@ -116,6 +111,33 @@
         });
 
     });
+
+    var mapOptions = {
+        zoom: 5,
+        center : {
+            lat: 29.426791,
+            lng: -98.489602
+        }
+    };
+    var mapCanvas = document.getElementById("map-canvas");
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var geocoder = new google.maps.Geocoder();
+    var address = {
+        address: "San Antonio, TX"
+};
+    function callback(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+            console.log(results);
+            map.setCenter(results[0].geometry.location);
+            var marker = new google.maps.Marker({
+                position: results[0].geometry.location,
+                map: map
+            });
+        } else {
+            alert("Geocoding was not successful - STATUS: " + status);
+        }
+    }
+    geocoder.geocode(address, callback);
 
 })();
 
