592b76cd29de8b67e792c74fc2b9d4c90293400f
Jake J <jakejohnson818@gmail.com>
2018-04-19 16:25:31 -0500

    Finished exercise

---
+++ js/map-filter-reduce.js
@@ -0,0 +1,50 @@
+const users = [
+    {
+        id: 1,
+        name: 'ryan',
+        email: 'ryan@codeup.com',
+        languages: ['clojure', 'javascript'],
+    },
+    {
+        id: 2,
+        name: 'luis',
+        email: 'luis@codeup.com',
+        languages: ['java', 'scala', 'php'],
+    },
+    {
+        id: 3,
+        name: 'zach',
+        email: 'zach@codeup.com',
+        languages: ['javascript', 'bash'],
+    },
+    {
+        id: 4,
+        name: 'fernando',
+        email: 'fernando@codeup.com',
+        languages: ['java', 'php', 'sql'],
+    },
+    {
+        id: 5,
+        name: 'justin',
+        email: 'justin@codeup.com',
+        languages: ['html', 'css', 'javascript', 'php'],
+    },
+];
+
+const threeLang = users.filter(function(user) {
+    return user.languages.length > 2;
+});
+
+const userEmail = users.map(function(user) {
+   return user.email;
+});
+
+const userId = users.reduce(function(id, user) {
+    id[user.id] = user.name;
+    return id;
+}, {});
+
+console.log(threeLang);
+console.log(userEmail);
+console.log(userId);
+
